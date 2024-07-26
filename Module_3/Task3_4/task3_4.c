5978714880ef47ae07bf0d740ec3b8538e10e467
ritzle <vladislav.ryazanov.study@gmail.com>
2024-07-26 13:42:08 +0700

    add write file task3_4

--- Module_3/Task3_4/task3_4.c
+++ Module_3/Task3_4/task3_4.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
 
     int pipefd[2];
 
-    // Создание трубы
+    // Создание канала
     if (pipe(pipefd) == -1) {
         perror("Ошибка pipe");
         exit(1);
@@ -31,36 +31,48 @@ int main(int argc, char *argv[]) {
     }
 
     if (pid == 0) { // Дочерний процесс
-        close(pipefd[0]); // Закрываем конец чтения
+        close(pipefd[0]); 
 
-        // Генерация и отправка чисел
         for (int i = 0; i < n; i++) {
-            int num = i + 1; // Генерация числа
+            int num = rand() % 255; 
             if (write(pipefd[1], &num, sizeof(num)) != sizeof(num)) {
                 perror("Ошибка write");
                 exit(1);
             }
         }
 
-        close(pipefd[1]); // Закрываем конец записи
+        close(pipefd[1]);
         exit(0);
-    } else { // Родительский процесс
-        close(pipefd[1]); // Закрываем конец записи
+    }
 
+    close(pipefd[1]);
+
+    char *filename = "text.txt";
+    FILE *fp = fopen(filename, "w");
+    if (fp) {
         // Чтение чисел
         for (int i = 0; i < n; i++) {
             int num;
+
             if (read(pipefd[0], &num, sizeof(num)) != sizeof(num)) {
                 perror("Ошибка read");
                 exit(1);
             }
-            printf("%d ", num); // Вывод полученного числа
-        }
-        printf("\n");
 
-        close(pipefd[0]); // Закрываем конец чтения
-        wait(NULL); // Ожидание завершения дочернего процесса
+            // Запись числа в файл
+            fprintf(fp, "%d\n", num);
+
+            printf("%d ", num); 
+        }
+
+        printf("\n");
+        fclose(fp);
+    } else {
+        perror("Ошибка открытия файла");
     }
+
+    close(pipefd[0]); 
+    wait(NULL); 
 
     return 0;
 }
